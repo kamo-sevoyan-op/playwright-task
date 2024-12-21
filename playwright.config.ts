9e96fed5cb950d55f9a6aa05bc7a3a83559b49e1
kamo-sevoyan-op <kamo.sevoyan@optimumpartners.co>
2024-12-21 22:28:22 +0400

    Set workers to 1, remove all comments.

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,25 +2,16 @@ import { defineConfig, devices } from "@playwright/test";
 
 export default defineConfig({
   testDir: "./tests",
-  /* Run tests in files in parallel */
   fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
   retries: 1,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  workers: 1,
   reporter: "html",
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
-    /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: "https://magento.softwaretestingboard.com",
   },
 
-  /* Configure projects for major browsers */
   projects: [
     {
       name: "chromium",
