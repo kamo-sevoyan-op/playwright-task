13cb41009bccfb6b56ebaa0229e9c3dddcd6b5d0
kamo-sevoyan-op <kamo.sevoyan@optimumpartners.co>
2024-12-24 16:28:35 +0400

    Replace double quotes with single quotes.

--- tests/product-page.spec.ts
+++ tests/product-page.spec.ts
@@ -1,59 +1,59 @@
-import { test, expect, Page, Locator } from "@playwright/test";
+import { test, expect, Page, Locator } from '@playwright/test';
 
 const NUM_PRODUCTS = 4;
 
 test.beforeEach(async ({ page }) => {
-  await page.goto("/");
+  await page.goto('/');
 });
 
 function toSnakeCase(productName: string) {
-  let parts = productName.trim().split(" ");
+  let parts = productName.trim().split(' ');
   parts = parts.map((part) => part.toLowerCase());
-  const result = parts.join("-");
+  const result = parts.join('-');
   return result;
 }
 
 function getProductByIndex(page: Page, index: number) {
-  const productContainer = page.locator(".widget-product-grid");
-  const product = productContainer.locator("li").nth(index);
+  const productContainer = page.locator('.widget-product-grid');
+  const product = productContainer.locator('li').nth(index);
   return product;
 }
 
 async function testProduct(
   page: Page,
   index: number,
-  linkType: "image" | "anchor"
+  linkType: 'image' | 'anchor'
 ) {
   const product = getProductByIndex(page, index);
-  const productLink = product.locator(".product-item-link");
+  const productLink = product.locator('.product-item-link');
   const productName = await productLink.textContent();
-  const productImage = product.locator(".product-image-photo");
+  const productImage = product.locator('.product-image-photo');
 
   expect(productName).toBeTruthy();
 
   let link: Locator | null = null;
-  if (linkType === "image") {
+  if (linkType === 'image') {
     link = productImage;
   } else {
     link = productLink;
   }
 
   await link.click();
-  await page.waitForLoadState("load");
+  await page.waitForLoadState('load');
 
-  const pageTitle = page.locator(".page-title .base");
+  const pageTitle = page.locator('.page-title .base');
   await expect(pageTitle).toHaveText(productName as string);
   const path = toSnakeCase(productName as string);
   await expect(page).toHaveURL(`/${path}.html`);
   await expect(page).toHaveTitle(productName as string);
 }
 
-test.describe("Product page tests", () => {
+test.describe('Product page tests', () => {
   for (let index = 0; index < NUM_PRODUCTS; index++) {
     test(`Should allow user to navigate to product page by product name, case ${index}`, async ({
       page,
     }) => {
-      await testProduct(page, index, "anchor");
+      await testProduct(page, index, 'anchor');
     });
   }
 
@@ -61,7 +61,7 @@ test.describe("Product page tests", () => {
     test(`Should allow user to navigate to product page by product image, case ${index}`, async ({
       page,
     }) => {
-      await testProduct(page, index, "image");
+      await testProduct(page, index, 'image');
     });
   }
 });
